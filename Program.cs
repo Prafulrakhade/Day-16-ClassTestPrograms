7c432672126a63caaf9bfb696e9df35c067bbd4c
Praful <prafulrakhade02@gmail.com>
2022-05-21 20:47:24 +0530

    [praful] Added : Class Test Program File Conducted By Rajesha Sir

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace ExamTestProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------WELCOME TO THE CLASS TEST PROGRAM---------------\n\n");
+            RegexProb prob = new RegexProb();
+
+            PerfectNumber pr = new PerfectNumber();
+
+            ReverseNumber rev = new ReverseNumber();
+
+            int[] intArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4, 5.5, 6.6 };
+            char[] charArray = { 'P', 'R', 'A', 'F', 'U', 'L' };
+
+            GenericExample generic = new GenericExample();
+            generic.getGen(intArray);
+            generic.getGen(doubleArray);
+            generic.getGen(charArray);
+            
+        }
+    }
+}
